f4a35cec0fcff44265b0483d7c43fb7701d7ead2
Shaw Lu <shawlu95@gmail.com>
2023-06-09 00:25:32 -0700

    create quiz brain

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -15,27 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    let questions = [
-        Question(q: "Four + two = six", a: "True"),
-        Question(q: "Five - three > 1", a: "True"),
-        Question(q: "Bailey is dog", a: "False"),
-        Question(q: "A slug's blood is green.", a: "True"),
-        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Question(q: "Google was originally called 'Backrub'.", a: "True"),
-        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    ]
-    
-    var idx = 0
-    var score = 0
-    
+    var quizBrain = QuizBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -43,8 +24,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        if sender.currentTitle == questions[idx].answer  {
-            score += 1
+        let userAnswer = sender.currentTitle! // force unwrap
+        let right = quizBrain.checkAnswer(userAnswer)
+        
+        if right {
             sender.backgroundColor = UIColor.green
         } else {
             sender.backgroundColor = UIColor.red
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,38 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by main on 6/9/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct QuizBrain {
+    let questions = [
+        Question(q: "Four + two = six", a: "True"),
+        Question(q: "Five - three > 1", a: "True"),
+        Question(q: "Bailey is dog", a: "False"),
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    
+    var idx = 0
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == questions[idx].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+}
