3d5f555729bf72842bcc35606162c166ed05ed0d
Shaw Lu <shawlu95@gmail.com>
2023-06-08 23:51:55 -0700

    use struct to store list of questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    struct Question {
+        let text: String
+        let answer: String
+        
+        init(q: String, a: String) {
+            text = q
+            answer = a
+        }
+    }
+    
+    let questions = [
+        Question(q: "Four + two = six", a: "True"),
+        Question(q: "Five - three > 1", a: "True"),
+        Question(q: "Bailey is dog", a: "False")
+    ]
+    
+    var idx = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        if sender.currentTitle == questions[idx].answer  {
+            score += 1
+            print("right")
+        } else {
+            print("wrong")
+        }
+            
+        idx += 1
+        if idx == questions.count {
+            idx = 0
+            score = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = questions[idx].text
+    }
 }
 
